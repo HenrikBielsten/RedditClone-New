d8b0f366349f1b8c13eee4c942a82e9397bc60b4
HenrikBielsten <h.bielsten@hotmail.se>
2018-01-07 16:14:29 +0100

    Password is now editable

--- app/auth/editProfile.php
+++ app/auth/editProfile.php
@@ -5,32 +5,27 @@ require __DIR__.'/../autoload.php';
 $id = (int)$_SESSION['user']['id'];
 
 // CHANGES THE NAME, USERNAME, EMAIL AND BIOGRAPHY
-// if (isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['biography'])) {
-//   $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-//   $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-//   $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-//   $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
-//
-//   $query = 'UPDATE users SET name = :name, username = :username, email = :email, biography = :biography WHERE id = :id';
-//
-//   $statement = $pdo->prepare($query);
-//
-//   $statement->bindParam(':name', $name, PDO::PARAM_STR);
-//   $statement->bindParam(':username', $username, PDO::PARAM_STR);
-//   $statement->bindParam(':email', $email, PDO::PARAM_STR);
-//   $statement->bindParam(':biography', $biography, PDO::PARAM_STR);
-//   $statement->bindParam(':id', $id, PDO::PARAM_INT);
-//
-//   $statement->execute();
-//
-//   redirect('../../pages/profile.php');
-//
-// }
-//
-// if (password_verify($password, $user["password"])) {
-// }
+if (isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['biography'])) {
+  $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+  $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+  $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+  $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
 
+  $query = 'UPDATE users SET name = :name, username = :username, email = :email, biography = :biography WHERE id = :id';
 
+  $statement = $pdo->prepare($query);
+
+  $statement->bindParam(':name', $name, PDO::PARAM_STR);
+  $statement->bindParam(':username', $username, PDO::PARAM_STR);
+  $statement->bindParam(':email', $email, PDO::PARAM_STR);
+  $statement->bindParam(':biography', $biography, PDO::PARAM_STR);
+  $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+  $statement->execute();
+
+  redirect('../../pages/profile.php');
+
+}
 
 // CHANGE PASSWORD
 if (isset($_POST['newPassword'])) {
@@ -39,12 +34,12 @@ if (isset($_POST['newPassword'])) {
 
   $passwordQuery = 'SELECT * FROM users WHERE id = :id';
 
-  $statement = $pdo->prepare($passwordQuery);
+  $passwordStatement = $pdo->prepare($passwordQuery);
 
-  $statement->bindParam(':id', $id, PDO::PARAM_INT);
-  $statement->execute();
+  $passwordStatement->bindParam(':id', $id, PDO::PARAM_INT);
+  $passwordStatement->execute();
 
-  $user = $statement->fetch(PDO::FETCH_ASSOC);
+  $user = $passwordStatement->fetch(PDO::FETCH_ASSOC);
 
   if (password_verify($currentPassword, $user['password'])) {
 
@@ -56,47 +51,13 @@ if (isset($_POST['newPassword'])) {
 
     $updateStatement = $pdo->prepare($updateQuery);
 
-    $statement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $updateStatement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+    $updateStatement->bindParam(':id', $id, PDO::PARAM_INT);
 
-    $statement->execute();
+    $updateStatement->execute();
 
     redirect('../../pages/profile.php');
 
   }
 
-};
-
-// // CHANGES THE NAME
-// if (isset($_POST['name'])) {
-//   $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-//
-//   $nameQuery = 'UPDATE users SET name = :name WHERE id = :id';
-//
-//   $nameStatement = $pdo->prepare($nameQuery);
-//
-//   $nameStatement->bindParam(':name', $name, PDO::PARAM_STR);
-//   $nameStatement->bindParam(':id', $id, PDO::PARAM_INT);
-//
-//   $nameStatement->execute();
-//
-//   redirect('../../pages/profile.php');
-//
-// }
-
-// // CHANGES THE USERNAME
-// if (isset($_POST['username'])) {
-//   $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-//
-//   $usernameQuery = 'UPDATE users SET username = :username WHERE id = :id';
-//
-//   $usernameStatement = $pdo->prepare($usernameQuery);
-//
-//   $usernameStatement->bindParam(':username', $username, PDO::PARAM_STR);
-//   $usernameStatement->bindParam(':id', $id, PDO::PARAM_INT);
-//
-//   $usernameStatement->execute();
-//
-//   redirect('../../pages/profile.php');
-//
-// }
+}
