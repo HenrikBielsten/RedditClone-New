2da8acb519c51f2d15b5fb75129eb6f7a9181203
HenrikBielsten <h.bielsten@hotmail.se>
2018-01-07 18:25:22 +0100

    Fixed so that there's no error when leaving image upload blank and saving changes

--- app/auth/editProfile.php
+++ app/auth/editProfile.php
@@ -4,7 +4,12 @@ require __DIR__.'/../autoload.php';
 
 $id = (int)$_SESSION['user']['id'];
 
-if (isset($_FILES['img'])) {
+
+
+
+// CHANE PROFILE PICTURE
+if (isset( $_FILES["img"]) && !empty($_FILES["img"]["name"])) {
+    if (is_uploaded_file( $_FILES["img"]["tmp_name"] ) && $_FILES["img"]["error"] === 0) {
   $path = pathinfo($_FILES['img']['name']);
   $extension = $path['extension'];
   $fileName = $_SESSION['user']['username'].'.'.$extension;
@@ -21,56 +26,57 @@ if (isset($_FILES['img'])) {
 
   move_uploaded_file($_FILES['img']['tmp_name'], __DIR__.'/../../images/'.$fileName);
 }
+}
 
-// // CHANGES THE NAME, USERNAME, EMAIL AND BIOGRAPHY
-// if (isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['biography'])) {
-//   $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-//   $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-//   $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-//   $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
-//
-//   $query = 'UPDATE users SET name = :name, username = :username, email = :email, biography = :biography WHERE id = :id';
-//
-//   $statement = $pdo->prepare($query);
-//
-//   $statement->bindParam(':name', $name, PDO::PARAM_STR);
-//   $statement->bindParam(':username', $username, PDO::PARAM_STR);
-//   $statement->bindParam(':email', $email, PDO::PARAM_STR);
-//   $statement->bindParam(':biography', $biography, PDO::PARAM_STR);
-//   $statement->bindParam(':id', $id, PDO::PARAM_INT);
-//
-//   $statement->execute();
-// }
-//
-// // CHANGE PASSWORD
-// if (isset($_POST['newPassword'])) {
-//
-//   $currentPassword = $_POST['currentPassword'];
-//
-//   $passwordQuery = 'SELECT * FROM users WHERE id = :id';
-//
-//   $passwordStatement = $pdo->prepare($passwordQuery);
-//
-//   $passwordStatement->bindParam(':id', $id, PDO::PARAM_INT);
-//   $passwordStatement->execute();
-//
-//   $user = $passwordStatement->fetch(PDO::FETCH_ASSOC);
-//
-//   if (password_verify($currentPassword, $user['password'])) {
-//
-//     $newPassword = filter_var($_POST['newPassword']);
-//
-//     $hashed_password = password_hash($newPassword, PASSWORD_DEFAULT);
-//
-//     $updateQuery = 'UPDATE users SET password = :password WHERE id = :id';
-//
-//     $updateStatement = $pdo->prepare($updateQuery);
-//
-//     $updateStatement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
-//     $updateStatement->bindParam(':id', $id, PDO::PARAM_INT);
-//
-//     $updateStatement->execute();
-//   }
-// }
+// CHANGES THE NAME, USERNAME, EMAIL AND BIOGRAPHY
+if (isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['biography'])) {
+  $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+  $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+  $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+  $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
+
+  $query = 'UPDATE users SET name = :name, username = :username, email = :email, biography = :biography WHERE id = :id';
+
+  $statement = $pdo->prepare($query);
+
+  $statement->bindParam(':name', $name, PDO::PARAM_STR);
+  $statement->bindParam(':username', $username, PDO::PARAM_STR);
+  $statement->bindParam(':email', $email, PDO::PARAM_STR);
+  $statement->bindParam(':biography', $biography, PDO::PARAM_STR);
+  $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+  $statement->execute();
+}
+
+// CHANGE PASSWORD
+if (isset($_POST['newPassword'])) {
+
+  $currentPassword = $_POST['currentPassword'];
+
+  $passwordQuery = 'SELECT * FROM users WHERE id = :id';
+
+  $passwordStatement = $pdo->prepare($passwordQuery);
+
+  $passwordStatement->bindParam(':id', $id, PDO::PARAM_INT);
+  $passwordStatement->execute();
+
+  $user = $passwordStatement->fetch(PDO::FETCH_ASSOC);
+
+  if (password_verify($currentPassword, $user['password'])) {
+
+    $newPassword = filter_var($_POST['newPassword']);
+
+    $hashed_password = password_hash($newPassword, PASSWORD_DEFAULT);
+
+    $updateQuery = 'UPDATE users SET password = :password WHERE id = :id';
+
+    $updateStatement = $pdo->prepare($updateQuery);
+
+    $updateStatement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+    $updateStatement->bindParam(':id', $id, PDO::PARAM_INT);
+
+    $updateStatement->execute();
+  }
+}
 
 redirect('../../pages/profile.php');
